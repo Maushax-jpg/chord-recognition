0e00ffa87f8bf703afb2588057b803f44de47d51
Maushax-jpg <maximilian.ederer@gmx.at>
2023-04-16 18:39:56 +0200

    implemented plot functions for Pitch space

--- circularPitchSpace.py
+++ circularPitchSpace.py
@@ -3,12 +3,11 @@ import matplotlib.pyplot as plt
 
 # chromatic index of a pitch
 # denoted as n_i
-chromatic_index = {-6:'gis',-5:'a',-4:'ais',-3:'h',-2:'c',-1:'cis',0:'d',1:'dis',2:'e',3:'f',4:'fis',5:'g'}
+chromatic_index = {-6:'gis',-5:'a',-4:'b',-3:'h',-2:'c',-1:'cis',0:'d',1:'es',2:'e',3:'f',4:'fis',5:'g'}
 
 # number of accidentals: flats (+) or sharps (-) in a key  (e.g. F-maj=-1,C-maj=0, G-Maj=1)
 # denoted as n_k
 num_accidentals = {-5:'Des',-4:'As',-3:'Es',-2:'B',-1:'F',0:'C',1:'G',2:'D',3:'A',4:'E',5:'H',6:'Fis/Ges'}
-
 
 def scaleVector(x,y,alpha):
     m,phi = cartesian2polar(x,y)
@@ -44,11 +43,11 @@ def getChromaticIndices():
     n_c = np.roll(list(chromatic_index.keys()),-4)
     return n_c
 
-def annotateLabel(axis,xy,note_label,ht_steps):
-    re,im = scaleVector(xy.real,xy.imag,1.1)
-    axis.text(re,im,note_label,rotation=np.arctan2(im,re)*180/np.pi-90,horizontalalignment='center',verticalalignment='center')
-    re,im = scaleVector(xy.real,xy.imag,0.85)
-    axis.text(re,im,ht_steps,rotation=np.arctan2(im,re)*180/np.pi-90,horizontalalignment='center',verticalalignment='center')
+def annotateLabel(axis,z,note_label,ht_steps):
+    x,y= scaleVector(z.real,z.imag,1.2)
+    axis.text(x,y,note_label,rotation=np.arctan2(y,x)*180/np.pi-90,horizontalalignment='center',verticalalignment='center')
+    x,y = scaleVector(z.real,z.imag,0.8)
+    axis.text(x,y,ht_steps,rotation=np.arctan2(y,x)*180/np.pi-90,horizontalalignment='center',verticalalignment='center')
 
 def plotHalftoneGrid(axis,n_ht):
     axis.plot([-1,1], [0,0], color='grey',linestyle=(0,(5,10)),linewidth=1)
@@ -58,17 +57,18 @@ def plotHalftoneGrid(axis,n_ht):
         ht = np.exp(-1j*2*np.pi*(i/n_ht))*1j
         axis.plot(ht.real,ht.imag,'o',color='grey',markersize=2)
 
-def plotCircleOfFifths(ax,plot_halftones=False):
-    plotHalftoneGrid(ax,84)
-    for n_k in num_accidentals:
-        # realtonigkeit plotten
-        rho_F = np.exp(-1j*2*np.pi*(7*n_k/84))*1j
-        ax.plot(rho_F.real,rho_F.imag,'o',color='k',markersize=4)
-        if plot_halftones:
-            annotateLabel(ax,rho_F,num_accidentals[n_k],7*n_k)
-        else:
-            annotateLabel(ax,rho_F,num_accidentals[n_k],None)
-    ax.set_title("Circle of Fifths")
+def plotCircleOfFifths(axis,chroma=None):
+    plotHalftoneGrid(axis,84)
+    for n_c in chromatic_index:
+        n_f = sym3(49*n_c,84,0)
+        rho_F = np.exp(-1j*2*np.pi*(n_f/84))*1j
+        axis.plot(rho_F.real,rho_F.imag,'o',color='k',markersize=4)
+        if n_c in chromatic_index.keys():
+            annotateLabel(axis,rho_F,chromatic_index[n_c],n_f)
+    axis.text(-1.1,1,"F",fontsize=10)
+    if chroma is not None:
+        plotChromaVector(axis,chroma,0,circle='F')
+    axis.axis('off')
 
 def checkIndex(n_f,n_k):
     if n_f is not None and -21 <= (n_f-7*n_k) and (n_f-7*n_k) <= 21:
@@ -76,10 +76,11 @@ def checkIndex(n_f,n_k):
     else:
         return False
     
-def plotKeyRelatedRealPitches(axis,n_k,type='fifths',plot_halftones=False):
-    if type=='fifths':
+def plotKeyRelatedRealPitches(axis,n_k=0,circle='F',chroma=None,plot_halftones=False):
+    if circle == 'FR':
+    # plot key related circle of fiths
         plotHalftoneGrid(axis,48)
-        axis.text(0,1.5,f"FR({num_accidentals[n_k]})",horizontalalignment='center',verticalalignment='center')
+        axis.text(-1,1.1,f"FR({num_accidentals[n_k]})",fontsize=10,horizontalalignment='center',verticalalignment='center')
         for n_c in chromatic_index.keys():
             n_f = sym3(49*n_c,84,7*n_k)
             # test ob tonigkeit im tonartbezogengen Kreis ist
@@ -91,10 +92,10 @@ def plotKeyRelatedRealPitches(axis,n_k,type='fifths',plot_halftones=False):
                     annotateLabel(axis,rho_FR,chromatic_index[n_c],n_fr)
                 else:
                     annotateLabel(axis,rho_FR,chromatic_index[n_c],None)
-
-    elif type=='thirds':
+        # plot key related circle of thirds
+    elif circle == 'TR':
         plotHalftoneGrid(axis,24)
-        axis.text(0,1.5,f"TR({num_accidentals[n_k]})",horizontalalignment='center',verticalalignment='center')
+        axis.text(-1,1.1,f"TR({num_accidentals[n_k]})",fontsize=10,horizontalalignment='center',verticalalignment='center')
         for n_c in chromatic_index:
             n_f = sym3(49*n_c,84,7*n_k)
             # check if pitch is in fifth related circle
@@ -106,9 +107,10 @@ def plotKeyRelatedRealPitches(axis,n_k,type='fifths',plot_halftones=False):
                     annotateLabel(axis,rho_TR,chromatic_index[n_c],n_tr)
                 else:
                     annotateLabel(axis,rho_TR,chromatic_index[n_c],None)
-    elif type=='diatonic':
+    elif circle =='DR':
+        # plot key related diatonic circle
         plotHalftoneGrid(axis,12)
-        axis.text(0,1.5,f"DR({num_accidentals[n_k]})",horizontalalignment='center',verticalalignment='center')
+        axis.text(-1,1.1,f"DR({num_accidentals[n_k]})",fontsize=10,horizontalalignment='center',verticalalignment='center')
         for n_c in chromatic_index:
             # map pitch index to circle of fifth
             n_f = sym3(49*n_c,84,7*n_k)
@@ -121,67 +123,83 @@ def plotKeyRelatedRealPitches(axis,n_k,type='fifths',plot_halftones=False):
                     annotateLabel(axis,rho_DR,chromatic_index[n_c],n_dr)
                 else:
                     annotateLabel(axis,rho_DR,chromatic_index[n_c],None)
-
-def plotChromaVector(axs,chroma,key_index,arrow_color='r'):
-    # plot F,FR,TR,DR ->
-    for ax in axs:
-        ax.set_xlim([-1,1])
-        ax.set_ylim([-1,1])
-    # circular shift of chroma vector, adjust index
-    rho_FR = np.zeros(1,dtype=complex)
-    rho_TR = np.zeros(1,dtype=complex)
-    rho_DR = np.zeros(1,dtype=complex)
-    # halftones are spaced around -6,5 -> create index
-    lst = np.linspace(0,11,12,dtype=int)
-    chroma_idx = []
-    for i,x in enumerate(np.roll(lst,0)):
-        if(i<=7):
-            chroma_idx.append(i-2)
-        else:
-            chroma_idx.append(i-14)
-    for i,x in enumerate(chroma):
-        n_f = sym3(49*chroma_idx[i],84,7*key_index)
-        if checkIndex(n_f,key_index):
-            # plot tonal components in circle of fifths
-            # plot is rotated by 90° according to definitions (multiply by 1j)
-            n_fr = sym(n_f-7*key_index, 48)
-            temp = x*np.exp(-1j*2*np.pi*(n_fr/48))*1j
-            axs[0].plot(temp.real,temp.imag,'x',markersize=4,color=arrow_color)
-            rho_FR +=temp
-            # plot tonal components in circle of thirds
-            n_tr = sym(n_f-7*key_index-12,24)
-            temp=x*np.exp(-1j*2*np.pi*((n_tr)/24))*1j
-            axs[1].plot(temp.real,temp.imag,'x',markersize=4,color=arrow_color)
-            rho_TR +=temp
-            # plot tonal components in diatonic circle
-            n_dr = sym(n_f-7*key_index,12)
-            temp=x*np.exp(-1j*2*np.pi*(n_dr/12))*1j
-            axs[2].plot(temp.real,temp.imag,'x',markersize=4,color=arrow_color)
-            rho_DR += temp
-    axs[0].quiver(0,0,rho_FR.real,rho_FR.imag, units='xy',scale=1,color=arrow_color)
-    axs[1].quiver(0,0,rho_TR.real,rho_TR.imag, units='xy',scale=1,color=arrow_color)
-    axs[2].quiver(0,0,rho_DR.real,rho_DR.imag, units='xy',scale=1,color=arrow_color)
+    if chroma is not None:
+        plotChromaVector(axis,chroma,n_k,circle=circle)
+    axis.axis('off')
         
+def plotChromaVector(axis,chroma,n_k,circle='F'):
+    axis.set_xlim([-1,1])
+    axis.set_ylim([-1,1])
+    chroma_idx = getChromaticIndices()
+    if circle == 'F':
+        rho_F = np.zeros(1,dtype=complex)
+        for i,chroma_bin in enumerate(chroma):
+            n_f = sym3(49*chroma_idx[i],84,0)
+            # calculate vector entries for circle of fifth
+            temp = chroma_bin * np.exp(-1j*2*np.pi*(n_f/84))*1j
+            axis.plot(temp.real,temp.imag,'xk')
+            rho_F += temp
+        axis.quiver(0,0,rho_F.real,rho_F.imag, units='xy',scale=1,color='r')
+    elif circle =='FR':
+        rho_FR = np.zeros(1,dtype=complex)
+        for i,chroma_bin in enumerate(chroma):
+            n_f = sym3(49*chroma_idx[i],84,7*n_k)
+            if checkIndex(n_f,n_k):
+                n_fr = sym(n_f-7*n_k, 48)
+                temp = chroma_bin*np.exp(-1j*2*np.pi*(n_fr/48))*1j
+                axis.plot(temp.real,temp.imag,'xk')
+                rho_FR +=temp 
+        axis.quiver(0,0,rho_FR.real,rho_FR.imag, units='xy',scale=1,color='r')
+    elif circle =='TR':
+        rho_TR = np.zeros(1,dtype=complex)
+        for i,chroma_bin in enumerate(chroma):
+            n_f = sym3(49*chroma_idx[i],84,7*n_k)
+            if checkIndex(n_f,n_k):
+                n_tr = sym(n_f-7*n_k-12,24)
+                temp = chroma_bin*np.exp(-1j*2*np.pi*((n_tr)/24))*1j
+                axis.plot(temp.real,temp.imag,'xk')
+                rho_TR +=temp
+        axis.quiver(0,0,rho_TR.real,rho_TR.imag, units='xy',scale=1,color='r')
+    elif circle == 'DR':
+        rho_DR = np.zeros(1,dtype=complex)
+        for i,chroma_bin in enumerate(chroma):
+            n_f = sym3(49*chroma_idx[i],84,7*n_k)
+            if checkIndex(n_f,n_k):
+                n_dr = sym(n_f-7*n_k,12)
+                temp = chroma_bin*np.exp(-1j*2*np.pi*(n_dr/12))*1j
+                axis.plot(temp.real,temp.imag,'xk')
+                rho_DR += temp
+        axis.quiver(0,0,rho_DR.real,rho_DR.imag, units='xy',scale=1,color='r')
+    axis.axis('off')
 
 def transformChroma(chroma):
+    rho_F  = np.zeros_like((1,chroma.shape[0]),dtype=complex)
     rho_FR = np.zeros_like(chroma,dtype=complex)
     rho_TR = np.zeros_like(chroma,dtype=complex)
     rho_DR = np.zeros_like(chroma,dtype=complex)
     chroma_index = getChromaticIndices()
+    # iterate over all time steps
     for time_index in range(chroma.shape[0]):
+        # calculte key related circles
         for key_index,n_k in enumerate(num_accidentals):
+            # iterate over all chroma bins with correct index
             for chroma_i,chroma_bin in zip(chroma_index,chroma[time_index,:]):
                 n_f = sym3(49*chroma_i,84,7*n_k)
-                if n_f:
+                if n_k == 0:
+                    # calculate vector entries for circle of fifth
+                    rho_F[time_index] += chroma_bin * np.exp(-1j*2*np.pi*(n_f/84))
+                # check if real pitch is part of the key n_k
+                if checkIndex(n_f,n_k):
                     n_fr = sym(n_f-7*n_k, 48)
-                if n_fr:
-                    rho_FR[time_index,key_index] += chroma_bin*np.exp(-1j*2*np.pi*(n_fr/48))*1j        
-                n_tr = sym(n_f-7*n_k-12,24)
-                rho_TR[time_index,key_index] += chroma_bin*np.exp(-1j*2*np.pi*(n_tr/24))*1j           
-                n_dr = sym(n_f-7*n_k,12)
-                rho_DR[time_index,key_index] += chroma_bin*np.exp(-1j*2*np.pi*(n_dr/12))*1j
-    keylabels = list(num_accidentals.values())
-    return (keylabels,rho_FR,rho_TR,rho_DR)
+                    # fifth related circle
+                    rho_FR[time_index,key_index] += chroma_bin*np.exp(-1j*2*np.pi*(n_fr/48))       
+                    n_tr = sym(n_f-7*n_k-12,24)
+                    # third related circle
+                    rho_TR[time_index,key_index] += chroma_bin*np.exp(-1j*2*np.pi*(n_tr/24))        
+                    # diatonic circle   
+                    n_dr = sym(n_f-7*n_k,12)
+                    rho_DR[time_index,key_index] += chroma_bin*np.exp(-1j*2*np.pi*(n_dr/12))
+    return (rho_F,rho_FR,rho_TR,rho_DR)
 
 if __name__=='__main__':
     fig,ax = plt.subplots(13,3,figsize=(5, 22),gridspec_kw={'width_ratios': [1, 1,1]})
