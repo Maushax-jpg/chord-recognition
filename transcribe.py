40c0460f09d9043e96ba2a058f8dd89da5077282
Maushax-jpg <maximilian.ederer@gmx.at>
2023-05-07 17:25:09 +0200

    using evaluation and chroma splits

--- transcribe.py
+++ transcribe.py
@@ -1,11 +1,11 @@
 import HMM
 import madmom
 import dataloader
+import evaluate
 import utils
 import mir_eval
 import matplotlib.pyplot as plt
 import numpy as np
-
 
 
 def createChordTemplates():
@@ -14,16 +14,16 @@ def createChordTemplates():
     return: list Chord_labels , ndarray templates (24x12)
     """
     pitch_class = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
-    chord_labels = ["N"]
-    templates = np.zeros((25,12),dtype=float)
+    chord_labels = []
+    templates = np.zeros((24,12),dtype=float)
     # major chords
     for i,x in enumerate(pitch_class):
         chord_labels.append(f"{x}:maj")
-        templates[i+1,:] = np.roll(mir_eval.chord.quality_to_bitmap('maj'),i)
+        templates[i,:] = np.roll(mir_eval.chord.quality_to_bitmap('maj'),i)
     # minor chords
     for i,x in enumerate(pitch_class):
         chord_labels.append(f"{x}:min")
-        templates[i+12+1,:] = np.roll(mir_eval.chord.quality_to_bitmap('min'),i)
+        templates[i+12,:] = np.roll(mir_eval.chord.quality_to_bitmap('min'),i)
     return chord_labels,templates
 
 def plotTemplates(labels,templates):
@@ -35,30 +35,25 @@ def plotTemplates(labels,templates):
     ax.set_yticklabels(labels);
     return fig
 
-if __name__=='__main__':
-
-    model_path = "/home/max/ET-TI/Masterarbeit/models/hmm_model.pkl"   
-    model = HMM.load_model(model_path)
-
-    dataset = dataloader.MIRDataset("beatles",use_deep_chroma=True,align_chroma=True)
-    track_ids = dataset.getTrackIDs()
-
-    y,t_chroma,chroma,ref_intervals,ref_labels = dataset[track_ids[104]]
-    
-    decoder = 'template'
-    if decoder == 'hmm':
-        # HMM prediction
-        est_intervals,est_labels = model.predict(t_chroma,chroma)
-    elif decoder == "dcp":
+def transcribeChromagram(t_chroma,chroma,transcriber="CRP"):
+    if transcriber == "CRP":
+        print("Transcribing with Chord Recognition Processor")
         crp = madmom.features.chords.DeepChromaChordRecognitionProcessor()
         prediction = crp(chroma)
-        # prediction ->  array([(tstart , stop, label), .. ,(tstart , stop, label)])
         est_intervals = np.array([[x[0],x[1]] for x in prediction])
         est_labels = [x[2] for x in prediction]
-    elif decoder == 'template':
+        return est_intervals,est_labels
+    elif transcriber == "HMM":
+        print("Transcribing with Hidden Markov Model")
+        model_path = "/home/max/ET-TI/Masterarbeit/models/hmm_model.pkl"   
+        model = HMM.load_model(model_path)
+        est_intervals,est_labels = model.predict(t_chroma,chroma)
+        return est_intervals,est_labels
+    elif transcriber == "TEMPLATE":
+        print("Transcribing with Chord Templates")
         labels,templates = createChordTemplates()
         max_vals = np.max(chroma, axis=1)
-        chroma_norm = chroma / max_vals[:,None]
+        chroma_norm = chroma / (max_vals[:,None]+np.finfo(float).eps)
         correlation = np.dot(templates,chroma_norm.T)
         est_labels_unfiltered = [labels[np.argmax(correlation[:,i])] for i in range(chroma.shape[0])]
         est_intervals = []
@@ -70,13 +65,27 @@ if __name__=='__main__':
                 est_intervals.append([t_start,t_chroma[i]])
                 t_start = t_chroma[i]
         est_intervals = np.array(est_intervals)
+        return est_intervals,est_labels
+    else:
+        raise ValueError(f"Invalid Transcriber {transcriber}")
+    
 
+if __name__=='__main__':
 
-    # visualize results
-    fig,ax = plt.subplots(2,1,figsize=(10,10))
-    interval = (0,15)
-    utils.plotAudioWaveform(ax[0],y,interval)
-    utils.plotAnnotations(ax[0],ref_intervals,ref_labels,interval)
-    utils.plotPredictionResult(ax[0],est_intervals,est_labels,interval)
-    utils.plotChroma(ax[1],chroma,interval)
-    plt.show()
+    dataset = dataloader.MIRDataset("beatles",use_deep_chroma=True,align_chroma=True,split_nr=8)
+   
+    tracks = dataset.getTrackList()
+    track_id = tracks.keys()
+
+    for id in track_id:
+        y,t_chroma,chroma,ref_intervals,ref_labels = dataset[id]
+        results = {}
+        for transcriber in ["CRP","TEMPLATE"]:
+            # transcribe
+            est_intervals,est_labels = transcribeChromagram(t_chroma,chroma,transcriber)
+             # evaluate
+            score,seg_score = evaluate.evaluateTranscription(est_intervals,est_labels,ref_intervals,ref_labels)
+            ref_evalmatrix,est_evalmatrix = evaluate.createEvalMatrix(t_chroma,est_intervals,est_labels,ref_intervals,ref_labels)
+            evaluate.plotEvaluationMatrix(ref_evalmatrix,est_evalmatrix)
+            P, R, F, TP, FP, FN = evaluate.compute_eval_measures(ref_evalmatrix,est_evalmatrix)
+            print(f"MajMin: {score}, MeanSeg: {seg_score}, P: {P}, R: {R}, F: {F}, TP: {TP}, FP: {FP}, FN: {FN}")
