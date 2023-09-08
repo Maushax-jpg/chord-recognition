88b2b7dc9ec5d7a4c06983098687e4437c5069b0
Maushax-jpg <maximilian.ederer@gmx.at>
2023-09-08 14:58:44 +0200

    worked on gating function
    It's not possible to eliminate a lot of key candidates. suggested: from 6 remaining keys pick 3 with most correlation and interval energy of the key related chromagram.

--- key_estimation.py
+++ key_estimation.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import mir_eval
 import pitchspace
-import itertools
-
 
 # figure related parameters
 SAVE = False
@@ -15,11 +13,11 @@ name = "key_estimation_pc_energy.png"
 basepath = "/home/max/ET-TI/Masterarbeit/latex/figures"
 
 start = 0
-stop = 10
+stop = 20
 path = "/home/max/ET-TI/Masterarbeit/mirdata/beatles/"
 # title = "06_-_Rubber_Soul/11_-_In_My_Life"
-# title = "12_-_Let_It_Be/06_-_Let_It_Be"
-title = "10CD1_-_The_Beatles/CD1_-_17_-_Julia"
+title = "12_-_Let_It_Be/06_-_Let_It_Be"
+# title = "10CD1_-_The_Beatles/CD1_-_17_-_Julia"
 audiopath = f"{path}/audio/{title}.wav"
 
 # the exception handling is necessary due to a weird bug, if i try to load it the first time it doesnt work!
@@ -65,9 +63,20 @@ if False:
         plt.savefig(basepath+"/"+name)
     plt.show()
 
+if False: # evaluate Chord Prototypes instead of chroma
+    triads,labels_triads = utilities.chordTemplates("triads")
+    tetrads, labels_tetrads = utilities.chordTemplates("tetrads")
+    templates = np.concatenate((triads,tetrads),axis=0)
+    t_templates = np.linspace(start,stop,templates.shape[0],endpoint=True)
+    labels = labels_triads + labels_tetrads
+
+    # overwrite chroma/annotations with prototype information
+    target = utilities.createChordIntervals(t_templates,labels)
+    t = t_templates
+    chroma = templates
 # other approach to calculate correlation with major key profiles (krumhansl)
 templates = np.zeros((12,12),dtype=float)
-key_profile = np.array([5, 2, 3.5, 2, 4.5, 4, 2, 4.5, 2, 3.5, 1.5, 4])/12 # (12,)
+key_profile = np.array([5, 2, 3.5, 2, 4.5, 4, 2, 4.5, 2, 3.5, 1.5, 4])/12 # (12,) arbitrary normation for plots
 # key_profile = key_profile / np.sum(key_profile)
 for i in range(12):
     templates[i,:] = np.roll(key_profile,i)
@@ -84,34 +93,38 @@ ic_categories = np.array([2,3,4])
 for pitch_class in range(12):
     key_related_chroma = np.multiply(chroma,np.roll(key_template,pitch_class))
     interval_categories[pitch_class,:,:] = features.intervalCategories(key_related_chroma)
-    ic_energy[:,pitch_class] = np.sum(interval_categories[pitch_class,:,:], axis=1)  # sum energy of m3/M6,M3/m6,P4/P5  
+    ic_energy[:,pitch_class] = np.sum(interval_categories[pitch_class,:,2:5], axis=1)  # sum energy of m3/M6,M3/m6,P4/P5  
 
-# we can savely discard 6 keys with lowest correlation as they are far away from the real key on the circle of fifths!
-row_indices = np.arange(correlation.shape[0])[:,np.newaxis]
+# we can savely discard some keys with low correlation (6 or 8?)
+correlation_energy = np.square(correlation)
+key_candidates = np.argsort(ic_energy+correlation_energy,axis=1)[:,-3:]
 
-# key_candidates = np.argsort(correlation,axis=1)[:,:6] # pick the 6 lowest correlation indices
-# correlation[row_indices, key_candidates] = 0 # set the correlation to zero
-key_candidates_corr = np.argsort(correlation,axis=1)[:,-3:] 
-key_candidates_ic = np.argsort(ic_energy,axis=1)[:,-3:] 
-
-
-fig,ax = plt.subplots(4,1,height_ratios=(1,3,3,2),figsize=(9,5))
-utilities.plotChordAnnotations(ax[0],target,(start,stop))
-utilities.plotChromagram(ax[1],t,chroma)
-utilities.plotChromagram(ax[2],t,correlation)
-utilities.plotChromagram(ax[3],t,ic_energy)
-ax[1].plot(t,key_candidates_corr,'xg',markersize=1)
-ax[1].plot(t,key_candidates_ic,'xb',markersize=1)
+# define gridspec and axes
+fig = plt.figure(figsize=(9,11))
+gs = matplotlib.gridspec.GridSpec(5, 3,width_ratios=(40,1,5),height_ratios=(1,6,6,6,6), hspace=0.2,wspace=0.05)
+ax00 = fig.add_subplot(gs[0,0])
+# ax01 = fig.add_subplot(gs[0,1])
+ax10 = fig.add_subplot(gs[1,0])
+ax11 = fig.add_subplot(gs[1,1])
+ax20 = fig.add_subplot(gs[2,0])
+ax21 = fig.add_subplot(gs[2,1])
+ax30 = fig.add_subplot(gs[3,0])
+ax31 = fig.add_subplot(gs[3,1])
+ax40 = fig.add_subplot(gs[4,0])
+ax41 = fig.add_subplot(gs[4,1])
+utilities.plotChordAnnotations(ax00,target,(start,stop))
+img = utilities.plotChromagram(ax10,t,chroma)
+fig.colorbar(img,cax=ax11)
+img = utilities.plotChromagram(ax20,t,correlation_energy,vmax=0.5)
+fig.colorbar(img,cax=ax21)
+ax20.text(0,12,"Correlation")
+img = utilities.plotChromagram(ax30,t,ic_energy,vmax=0.5)
+fig.colorbar(img,cax=ax31)
+ax30.text(0,12,"IC-Energy")
+img = utilities.plotChromagram(ax40,t,correlation_energy+ic_energy,vmax=0.5)
+ax40.text(0,12,"Sum")
+fig.colorbar(img,cax=ax41)
+ax10.plot(t,key_candidates,'ok',markersize=1)
 plt.show()
 
-# r_F,_,_,_ = pitchspace.transformChroma(chroma)
-# keys = np.argsort(pc_energy,axis=1)[:,-3:]
-
-# colors = ["blue","lightblue", "green", "red", "orange", "purple", "grey", "lightgreen","brown", "magenta", "teal","cyan"]
-# for i in range(r_F.shape[0]):
-#     # rotate vector for plot
-#     #z = r_F[i] * 1j
-#     ax[2].plot(t[i],keys[i,0],'o',color='grey', markersize=1)
-#     ax[2].plot(t[i],keys[i,1],'o',color='grey', markersize=1)
-#     ax[2].plot(t[i],keys[i,2],'o',color='grey', markersize=1)
 
