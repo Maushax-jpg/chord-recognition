11699fe4098156d1e47443e6bf2d1506e220a0a3
Maushax-jpg <maximilian.ederer@gmx.at>
2023-12-09 16:12:03 +0100

    transcriber for chord recognition experiment

--- transcribe.py
+++ transcribe.py
@@ -3,6 +3,12 @@ import argparse
 import dataloader
 import utils
 import features
+import numpy as np
+import plots
+import h5py
+from tqdm import tqdm
+from datetime import datetime
+import json
 
 def parse_arguments():
     """extract command line arguments in ordert to setup chord recognition pipeline"""
@@ -12,7 +18,7 @@ def parse_arguments():
     script_directory = os.path.dirname(os.path.abspath(__file__))
     default_path = os.path.join(script_directory, "mirdata")
 
-    parser.add_argument('--filepath',type=str,default=None,help="set path to audiofile or ommit to transcribe a dataset")
+    parser.add_argument('result_file',help="specify filename to save the results")
     parser.add_argument('--dataset_path',type=str,default=default_path,help="set path to datasets")
     parser.add_argument('--dataset',choices=['beatles','rwc_pop','rw','queen'],
                         default=['beatles','rwc_pop','rw','queen'],
@@ -31,44 +37,108 @@ def parse_arguments():
                          help='Select source separation type'
                          )
     parser.add_argument('--prefilter', choices=[None, 'median', 'rp'], default='median', help='Select Prefilter type')
-    parser.add_argument('--median_length', type=int, default=7, help='Prefilter length')
-    parser.add_argument('--rp_embedding', type=int, default=25, help='Embedding value')
-    parser.add_argument('--rp_neighbours', type=int, default=50, help='Neighbours value')
+    parser.add_argument('--prefilter_length', type=int, default=7, help='Prefilter length')
+    parser.add_argument('--embedding', type=int, default=25, help='Embedding value')
+    parser.add_argument('--neighbors', type=int, default=50, help='Neighbours value')
     parser.add_argument('--postfilter', choices=[None, 'hmm', 'median'], default='hmm', help='select Postfilter type')
     parser.add_argument('--transition_prob', type=float, default=0.3, help='self-transition probability for a chord')
     parser.add_argument('--postfilter_length', type=int, default=4, help='Postfilter length')
-
+    parser.add_argument('--display',choices=[True,False],default=False)
     args = parser.parse_args()
 
     # Convert Namespace to dictionary
     return vars(args)
 
+def addDataset(group,dataset_name,data,metadata={}):
+    """create a dataset entry in the given group of the hdf5 file"""
+    try:
+        dset = group.create_dataset(dataset_name,data=data)
+    except ValueError as error: 
+        print(f"Error while creating dataset {dataset_name}:{error}\moving on..")
+        return
+    for key,value in metadata.items():
+        dset.attrs.create(str(key), value)
+    return dset
+
+def saveResults(group,track_id,scores={},track_data={}):
+    """saves the results for a given track_id in hdf5 format.
+     scores: a dictionary containing evaluation results
+                e.g. scores = {"majmin":0.78,"seg_score":0.86}
+     data:   a dictionary containing the datasets that need to be stored
+                e.g. track_data = {"dataset_name": (mydata,mymetadata)}
+    """
+    # create a subgroup for the track_id
+    subgroup = group.create_group(f"{track_id}")
+    # store the evaluation scores as metadata of the group
+    for eval_scheme,score in scores.items():
+        subgroup.attrs.create(f"{eval_scheme}",score)
+
+    for dataset_name,(data,metadata) in track_data.items():
+        addDataset(subgroup,dataset_name,data,metadata)
+
 if __name__ == "__main__":
+
+    #### chord recognition experiment #### 
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H:%M:%S') 
     params = parse_arguments()
-    
-    filepath = params["filepath"]
-    if filepath is not None:
-        # transcribe song
-        print(filepath)
-        quit()
+    fpath = params["result_file"]+timestamp+".hdf5"
+    file = h5py.File(fpath, 'w')
+    print(f"created file {fpath}")
+    file.create_dataset('parameters', data=json.dumps(params))
 
-
-    # Experimental pipeline
-    from tqdm import tqdm
-    print(f"Starting Transcription!")
-    for name in params["dataset"]:
-        dataset = dataloader.Dataloader(name,base_path=params["dataset_path"],source_separation=params["source_separation"])
-        print(f"Dataset: {name}")
+    for datasetname in params["dataset"]:
+        dataset = dataloader.Dataloader(datasetname,base_path=params["dataset_path"],source_separation=params["source_separation"])
+        grp = file.create_group(datasetname)
         for fold in range(1,9):
-            for track_id in tqdm(dataset.getExperimentSplits(fold),desc=f"fold {fold}/8"):  
-                
-                # Load audiofile
+            for track_id in tqdm(dataset.getExperimentSplits(fold),desc=f"fold {fold}/8"): 
+                # Load audiofile and ground truth annotations
                 name = dataset.getTitle(track_id)
+
                 filepath,annotationpath = dataset[track_id]
                 ref_intervals,ref_labels = utils.loadChordAnnotations(annotationpath)
                 y = utils.loadAudiofile(filepath)
+                gt = utils.loadChordAnnotations(annotationpath)
 
                 # compute chromagram
-                chroma = features.crpChroma(y)
+                chroma_params = {"nCRP":33,"hop_length":2048,"fs":22050}
+                chroma = features.crpChroma(y,nCRP=33,liftering=True,window=False)
                 t_chroma = utils.timeVector(chroma.shape[1],hop_length=2048)
-                #est_intervals, est_labels = transcribeChromagram(t_chroma,chroma,**parameters)    
+                
+                # apply prefilter
+                filter_type = params.get("prefilter")
+                chroma_smoothed = features.applyPrefilter(t_chroma,chroma,filter_type,**params)
+
+                # apply RMS thresholding
+                rms_db = features.computeRMS(y)
+                chroma_smoothed[:,rms_db < -60] = -1/12
+
+                ## pattern matching ##         
+                vocabulary = params.get("vocabulary")
+                correlation,labels = features.computeCorrelation(chroma_smoothed,vocabulary)
+                
+                ## postfilter ##
+                filter_type = params.get("postfilter")
+                correlation_smoothed = features.applyPostfilter(correlation,labels,filter_type,**params)
+                
+                ## decode correlation matrix
+                chord_sequence = [labels[i] for i in np.argmax(correlation_smoothed,axis=0)]    
+                est_intervals,est_labels = utils.createChordIntervals(t_chroma,chord_sequence)   
+
+                ## Evaluation ##
+                eval_scheme = params.get("eval_scheme")
+                score,seg_score = utils.evaluateTranscription(est_intervals,est_labels,ref_intervals,ref_labels,eval_scheme)
+                
+                # save results to file
+                scores = {"majmin":score,"seg":seg_score}
+                data = {"est_intervals":(est_intervals,{}),
+                        "est_labels":(est_labels,{}),
+                        "chroma":(chroma,chroma_params),
+                        "chroma_enhanced":(chroma_smoothed,chroma_params)
+                        }
+                if params.get("display"):
+                    plots.plotResults(t_chroma,chroma, chroma_smoothed, ref_intervals, ref_labels,
+                                                est_intervals, est_labels)
+                saveResults(grp,track_id,scores,data)
+
+    file.close()
+    print(f"DONE")
--- utils.py
+++ utils.py
@@ -53,3 +53,78 @@ def timeVector(N,t_start=0,t_stop=None,hop_length=512,sr=22050):
     else:
         time = np.linspace(t_start,t_stop,N,endpoint=False)
     return time
+
+def createChordIntervals(t,labels):
+    """create chord intervals from a given sequence of labels by grouping them together+
+    returns nd.array,chord_labels
+    """
+    est_labels = []
+    est_intervals = []
+    t_start = t[0]
+    for time,label in zip(itertools.pairwise(t),itertools.pairwise(labels)):
+        if(label[0] != label[1]): # chord change
+            est_intervals.append([t_start,time[1]])
+            est_labels.append(label[0])
+            t_start = time[1]
+        else: # no chord change
+            continue
+    if t_start != time[1]: # process last chord change
+        est_intervals.append([t_start,time[-1]])
+        est_labels.append(label[1])
+    return np.array(est_intervals),est_labels
+
+def evaluateTranscription(est_intervals,est_labels,ref_intervals,ref_labels,scheme="majmin"):
+    """evaluate Transcription score according to MIREX scheme"""
+    est_intervals, est_labels = mir_eval.util.adjust_intervals(
+            est_intervals, est_labels, ref_intervals.min(),
+            ref_intervals.max(), mir_eval.chord.NO_CHORD,
+            mir_eval.chord.NO_CHORD)
+    (intervals,ref_labels,est_labels) = mir_eval.util.merge_labeled_intervals(
+        ref_intervals, ref_labels, est_intervals, est_labels)
+    durations = mir_eval.util.intervals_to_durations(intervals)
+    if scheme == "majmin":
+        comparisons = mir_eval.chord.majmin(ref_labels, est_labels)
+    elif scheme == "triads":
+        comparisons = mir_eval.chord.triads(ref_labels, est_labels)
+    elif scheme == "tetrads":
+        comparisons = mir_eval.chord.tetrads(ref_labels, est_labels)
+    elif scheme == "majmin_sevenths":
+        comparisons = mir_eval.chord.tetrads(ref_labels, est_labels)
+    score = round(mir_eval.chord.weighted_accuracy(comparisons, durations),2)
+    mean_seg_score = round(mir_eval.chord.seg(ref_intervals, est_intervals),2)
+    return score,mean_seg_score
+
+def createChordTemplates(template_type="majmin"):
+    """create a set of chord templates for the given evaluation scheme:
+
+        majmin: "maj","min"
+        triads: "maj","min","dim","aug"
+        triads_extended: "maj","min","dim","aug","sus2","sus4"
+        majmin_sevenths: "maj","min","maj7","7","min7"
+
+        returns templates,chord_labels
+    """
+    if template_type == "majmin":
+        quality = ["maj","min"]
+    elif template_type == "triads":
+        quality = ["maj","min","dim","aug"]
+    elif template_type == "triads_extended":    
+        quality = ["maj","min","dim","aug","sus2","sus4"]
+    elif template_type == "majmin_sevenths":
+        quality = ["maj","min","maj7","7","min7"]
+    templates = np.zeros((12,12*len(quality)+1),dtype=float)
+    chord_labels = []
+    chord_index = 0
+
+    # create chord templates + No chord prototype
+    for q in quality:  
+        template = mir_eval.chord.quality_to_bitmap(q)
+        template = template / np.sum(template)
+        for pitch in pitch_classes:
+            templates[:,chord_index] = np.roll(template,pitch.pitch_class_index)
+            chord_labels.append(f"{pitch.name}:{q}")
+            chord_index += 1
+    for i in range(12):
+        templates[i,chord_index] = -1/12
+    chord_labels.append("N")
+    return templates,chord_labels
