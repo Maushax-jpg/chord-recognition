b74fb16200718d254e0b810a68d397ef8d818da9
Maushax-jpg <maximilian.ederer@gmx.at>
2023-12-15 13:26:52 +0100

    included markdown tables as preview

--- evaluate.py
+++ evaluate.py
@@ -1,21 +1,27 @@
 import ipywidgets
-import IPython.display
+from IPython.display import display,Markdown
 import h5py
 import os
 import plots
 import utils
+import numpy as np
 from collections import namedtuple
 
 trackdata = namedtuple('track','track_id name dataset majmin_wscr majmin_seg majmin_f sevenths_wscr sevenths_seg sevenths_f')
 """named tuple to store track specific metadata"""
 
 def load_results(filepath):
-    """reads all scores in a .hdf5 file and returns a dictionary"""
+    """reads all scores in a .hdf5 file and returns a list of trackdata and a list of the used datasets"""
     results = []
     with  h5py.File(filepath,"r") as file:
         datasets = file.attrs.get("dataset")
+
         if datasets is None:
-            raise KeyError("Corrupt File: No Metadata indicating used datasets available..")
+            raise KeyError("Corrupt result file! no datasets are specified in the header!")
+        
+        if not isinstance(datasets,list): # convert to list if necessary
+            datasets = list(datasets)
+
         for grp_name in datasets:
             for subgrp_name in file[f"{grp_name}/"]:
                 subgrp = file[f"{grp_name}/{subgrp_name}"]
@@ -31,13 +37,50 @@ def load_results(filepath):
                         sevenths_f=subgrp.attrs.get("sevenths_f")
                     )
                 )
-    return results
+    return results,datasets
+
+
+def load_trackdata(filepath,track_id,dataset):
+    """reads out trackdata, chromagram and annotations for a given track from an .hdf5 file"""
+    with h5py.File(filepath,"r") as file:
+        subgrp = file[f"{dataset}/{track_id}"]
+
+        track_data = trackdata( 
+                        track_id=track_id,
+                        name=subgrp.attrs.get("name"),
+                        dataset=dataset,
+                        majmin_wscr=subgrp.attrs.get("majmin_score"),
+                        majmin_seg=subgrp.attrs.get("majmin_segmentation"),
+                        majmin_f=subgrp.attrs.get("majmin_f"),
+                        sevenths_wscr=subgrp.attrs.get("sevenths_score"),
+                        sevenths_seg=subgrp.attrs.get("sevenths_segmentation"),
+                        sevenths_f=subgrp.attrs.get("sevenths_f")
+                    )
+        
+        # load chroma to create time_vector from dataset
+        chroma = subgrp.get("chroma")
+        t_chroma = utils.timeVector(chroma.shape[1],hop_length=chroma.attrs.get("hop_length"))
+        
+        # strings have to be decode with utf-8 
+    
+        ref_labels = [x.decode("utf-8") for x in subgrp.get("ref_labels")]
+        majmin_labels = [x.decode("utf-8") for x in subgrp.get("majmin_labels")]
+        sevenths_labels = [x.decode("utf-8") for x in subgrp.get("sevenths_labels")]
+
+        # convert to numpy arrays and pack to tuples
+        chromadata = t_chroma,np.copy(chroma),np.copy(subgrp.get("chroma_prefiltered"))
+        ground_truth = np.copy(subgrp.get("ref_intervals")), ref_labels
+        est_majmin = np.copy(subgrp.get("majmin_intervals")),majmin_labels
+        est_sevenths = np.copy(subgrp.get("sevenths_intervals")), sevenths_labels
+    return track_data,chromadata,ground_truth,est_majmin,est_sevenths
+
 class hdf5GUI():
     """A simple ipython GUI to visualize data of a result file """
     def __init__(self,filepath=""):
         self.path = filepath
         self.current_figure = None
-        self.result_file = None
+        self.trackdata = []
+        self.datasets = []
         self.initializeGUI()
 
     def initializeGUI(self):
@@ -64,65 +107,58 @@ class hdf5GUI():
         self.dropdown_dataset.observe(self.update_dataset,'value')
         self.dropdown_id.observe(self.update_selected_track_id, 'value')
         self.button_load.on_click(self.load_track)
-        IPython.display.display(self.filepaths,self.output)
+        display(self.filepaths,self.output)
 
     def load_result_file(self,*args):
-        IPython.display.display(self.filepaths,self.selection,self.output,clear=True)
+        display(self.filepaths,self.selection,self.output,clear=True)
+        self.output.clear_output()
         try:
-            self.result_file = h5py.File(os.path.join(self.path,self.dropdown_resultfile.value),"r")
-        except Exception as e:
-            print(f"error loading file:{os.path.join(self.path,self.dropdown_resultfile.value)}\n{e}") 
-        with self.output:
-            self.output.clear_output()
-            self.preview_result_file()
-            self.dropdown_dataset.options = [x for x in self.result_file] # datasets are groups of hdf5 files
+            self.trackdata,self.datasets = load_results(os.path.join(self.path,self.dropdown_resultfile.value))
+            self.dropdown_dataset.options = self.datasets
             self.dropdown_dataset.value = self.dropdown_dataset.options[0]
-            self.update_dataset() # update dropdown widgets
+            self.update_dataset() # updates all dropdown widgets
+            self.preview_result_file()
+
+        except KeyError:
+            print("Corrupt File: No Metadata indicating used datasets available..")
+        except FileNotFoundError:
+            print("File not found.")
+        except IOError as e:
+            print(f"An error occurred while processing the file: {e}")
+
 
     def preview_result_file(self):
         """prints out an overview of the groups and subgroups of the loaded hdf5 file"""
-        print("Metadata:")
-        for m in self.result_file.attrs.keys():
-            print(f"{m} -> {self.result_file.attrs[m]}")
-        print("\nDatasets:\n    Track_ids:\n")
-        for grp in self.result_file:
-            print(f"- {grp}")
-            n = 0
-            for subgrp in self.result_file[grp]:
-                print(f"    - {subgrp}")
-                for subsubgrp in self.result_file[f"{grp}/{subgrp}"]:
-                    print(f"      {subsubgrp}")
-                n += 1
-                if n > 2:
-                    print(f"    :")
-                    break
+        majmin_f = [x.majmin_f for x in self.trackdata]
+        majmin_wscr =  [x.majmin_wscr for x in self.trackdata]
+        majmin_seg = [x.majmin_seg for x in self.trackdata]
+
+        sevenths_f = [x.sevenths_f for x in self.trackdata]
+        sevenths_wscr =  [x.sevenths_wscr for x in self.trackdata]
+        sevenths_seg = [x.sevenths_seg for x in self.trackdata]
+        table_md = "# Evaluation Results on the combined dataset\n\n"
+        table_md += "|eval-scheme| f-score| WSCR| Segmentation |\n| --- | --- |--- |--- |\n"
+        table_md += f"|majmin|{np.mean(majmin_f):0.2f}+/-{np.std(majmin_f):0.2f}|{np.mean(majmin_wscr):0.2f}+/-{np.std(majmin_wscr):0.2f}"
+        table_md += f"|{np.mean(majmin_seg):0.2f}+/-{np.std(majmin_seg):0.2f}|\n"
+        table_md += f"|sevenths|{np.mean(sevenths_f):0.2f}+/-{np.std(sevenths_f):0.2f}|{np.mean(sevenths_wscr):0.2f}+/-{np.std(sevenths_wscr):0.2f}"
+        table_md += f"|{np.mean(sevenths_seg):0.2f}+/-{np.std(sevenths_seg):0.2f}|\n"
+        with self.output:
+            display(Markdown(table_md))
+
 
     def load_track(self,*args):
         if self.dropdown_resultfile.value is not None:
-            IPython.display.display(self.filepaths,self.selection,self.output,clear=True)
+            display(self.filepaths,self.selection,self.output,clear=True)
+            trackdata,chromadata,ground_truth,est_majmin,est_sevenths = load_trackdata(os.path.join(self.path,self.dropdown_resultfile.value),
+                                                                             self.dropdown_id.value,self.dropdown_dataset.value)
             with self.output:
                 self.output.clear_output()
-                subgrp = self.result_file[f"{self.dropdown_dataset.value}/{self.dropdown_id.value}"]
-
-                # print out metadata for track
-                for k,v in subgrp.attrs.items():
-                    print(k,v)
-                
-                # plot results
-                chroma = subgrp.get("chroma")
-                t_chroma = utils.timeVector(chroma.shape[1],hop_length=chroma.attrs.get("hop_length"))
-                chroma_prefiltered = subgrp.get("chroma_prefiltered")
-                ref_intervals = subgrp.get("ref_intervals")
-                ref_labels = subgrp.get("ref_labels")
-                majmin_intervals = subgrp.get("majmin_intervals")
-                majmin_labels = subgrp.get("majmin_labels")
-                sevenths_intervals = subgrp.get("sevenths_intervals")
-                sevenths_labels = subgrp.get("sevenths_labels")
-                # convert labels 
-                ref_labels = [x.decode("utf-8") for x in ref_labels]
-                majmin_labels = [x.decode("utf-8") for x in majmin_labels]
-                sevenths_labels = [x.decode("utf-8") for x in sevenths_labels]
-                plots.plotResults(t_chroma,chroma,chroma_prefiltered,ref_intervals,ref_labels,majmin_intervals,majmin_labels)
+                table_md = f"## Evaluation results for {trackdata.name}\n\n"
+                table_md += "|eval-scheme| f-score| WSCR| Segmentation |\n| --- | --- |--- |--- |\n"
+                table_md += f"|majmin|{trackdata.majmin_f:0.2f}|{trackdata.majmin_wscr:0.2f}|{trackdata.majmin_seg:0.2f}|\n"
+                table_md += f"|sevenths|{trackdata.sevenths_f:0.2f}|{trackdata.sevenths_wscr:0.2f}|{trackdata.sevenths_seg:0.2f}|\n"
+                display(Markdown(table_md))
+                plots.plotResults(chromadata[0],chromadata[1],chromadata[2],ground_truth[0],ground_truth[1],est_majmin[0],est_majmin[1])
 
     def update_dataset(self,*args):
         """callback function for dropdown_dataset"""
@@ -130,11 +166,12 @@ class hdf5GUI():
         self.dropdown_id.value = self.dropdown_id.options[0] # set default track_id when changing dataset
 
     def update_dropdown_id_options(self,*args):
-        self.dropdown_id.options = [x for x in self.result_file[self.dropdown_dataset.value]]
+        self.dropdown_id.options = [x.track_id for x in self.trackdata if x.dataset == self.dropdown_dataset.value]
         self.update_selected_track_id()
 
     def update_selected_track_id(self,*args):
         if self.dropdown_id.value is not None:
             self.textbox_track_id.value = str(self.dropdown_id.value)
+            # print out name
         else:
             self.textbox_track_id.value = "None"
