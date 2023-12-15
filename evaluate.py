757c34f01fac76bd4ec4e61cc43310ad5ba3286e
Maushax-jpg <maximilian.ederer@gmx.at>
2023-12-15 19:54:14 +0100

    finetuned controls

--- evaluate.py
+++ evaluate.py
@@ -85,31 +85,51 @@ class hdf5GUI():
 
     def initializeGUI(self):
         self.output = ipywidgets.Output()
+
+        # controls for selecting a file
+        # TODO: check if valid resultfiles can be found in the given folder!
+        # TODO: also check for subfolders!
         self.dropdown_resultfile = ipywidgets.Dropdown(options=os.listdir(self.path),
                                             value = None,description='File:',
                                             layout=ipywidgets.Layout(width='50%'),
                                             disabled=False)
         self.button_loadfile = ipywidgets.Button(description='Load File')
 
+        # controls for selecting a track
         self.dropdown_dataset = ipywidgets.Dropdown(options=["beatles","rwc_pop","rw","queen"],
                                             value = "beatles",description='Dataset:',
                                             layout=ipywidgets.Layout(width='20%'),
                                             disabled=False)
         self.dropdown_id = ipywidgets.Dropdown(description='Track ID:',disabled=False,layout=ipywidgets.Layout(width='20%'))
-        self.textbox_track_id = ipywidgets.Text(description='',disabled=True)
-        self.button_load = ipywidgets.Button(description='Load Track')
+        self.textbox_name = ipywidgets.Text(description='',disabled=True)
+        
+        # controls for displaying a plot
+        self.t_start_slider = ipywidgets.IntSlider(min=0,step=5,readout_format='d',description="T0:")
+        self.button_plot = ipywidgets.Button(description='create Plot')
 
+        # create layout
         self.filepaths = ipywidgets.HBox([self.dropdown_resultfile,self.button_loadfile])
-        self.selection = ipywidgets.HBox([self.dropdown_dataset, self.dropdown_id,self.textbox_track_id, self.button_load])
-
+        self.selection = ipywidgets.HBox([self.dropdown_dataset, self.dropdown_id,self.textbox_name,self.t_start_slider, self.button_plot])
+        
         # register callback functions
         self.button_loadfile.on_click(self.load_result_file)
         self.dropdown_dataset.observe(self.update_dataset,'value')
         self.dropdown_id.observe(self.update_selected_track_id, 'value')
-        self.button_load.on_click(self.load_track)
+        self.button_plot.on_click(self.plot_results)
+
         display(self.filepaths,self.output)
 
+    def plot_results(self,*args):
+        display(self.filepaths,self.selection,self.output,clear=True)
+        track_data,chromadata,ground_truth,majmin_estimation,sevenths_estimation = self.trackdata
+        plots.plotResults(chromadata,ground_truth,majmin_estimation,sevenths_estimation,time_interval=(self.t_start_slider.value,self.t_start_slider.value+20)) 
+
+
     def load_result_file(self,*args):
+        if self.dropdown_resultfile.value is None:
+            with self.output:
+                print("please select file!")
+            return
         display(self.filepaths,self.selection,self.output,clear=True)
         self.output.clear_output()
         try:
@@ -132,7 +152,6 @@ class hdf5GUI():
         majmin_f = [x.majmin_f for x in self.trackdata]
         majmin_wscr =  [x.majmin_wscr for x in self.trackdata]
         majmin_seg = [x.majmin_seg for x in self.trackdata]
-
         sevenths_f = [x.sevenths_f for x in self.trackdata]
         sevenths_wscr =  [x.sevenths_wscr for x in self.trackdata]
         sevenths_seg = [x.sevenths_seg for x in self.trackdata]
@@ -142,28 +161,31 @@ class hdf5GUI():
         table_md += f"|{np.mean(majmin_seg):0.2f}+/-{np.std(majmin_seg):0.2f}|\n"
         table_md += f"|sevenths|{np.mean(sevenths_f):0.2f}+/-{np.std(sevenths_f):0.2f}|{np.mean(sevenths_wscr):0.2f}+/-{np.std(sevenths_wscr):0.2f}"
         table_md += f"|{np.mean(sevenths_seg):0.2f}+/-{np.std(sevenths_seg):0.2f}|\n"
+
         with self.output:
             display(Markdown(table_md))
 
+    def display_trackdata(self,trackdata):
+        table_md = "|eval-scheme| f-score| WSCR| Segmentation |\n| --- | --- |--- |--- |\n"
+        table_md += f"|majmin|{trackdata.majmin_f:0.2f}|{trackdata.majmin_wscr:0.2f}|{trackdata.majmin_seg:0.2f}|\n"
+        table_md += f"|sevenths|{trackdata.sevenths_f:0.2f}|{trackdata.sevenths_wscr:0.2f}|{trackdata.sevenths_seg:0.2f}|\n"
+        self.output.clear_output()
+        with self.output:
+            display(Markdown(table_md))
 
-    def load_track(self,*args):
-        if self.dropdown_resultfile.value is not None:
-            display(self.filepaths,self.selection,self.output,clear=True)
-            trackdata,chromadata,ground_truth,est_majmin,est_sevenths = load_trackdata(os.path.join(self.path,self.dropdown_resultfile.value),
-                                                                             self.dropdown_id.value,self.dropdown_dataset.value)
-            with self.output:
-                self.output.clear_output()
-                table_md = f"## Evaluation results for {trackdata.name}\n\n"
-                table_md += "|eval-scheme| f-score| WSCR| Segmentation |\n| --- | --- |--- |--- |\n"
-                table_md += f"|majmin|{trackdata.majmin_f:0.2f}|{trackdata.majmin_wscr:0.2f}|{trackdata.majmin_seg:0.2f}|\n"
-                table_md += f"|sevenths|{trackdata.sevenths_f:0.2f}|{trackdata.sevenths_wscr:0.2f}|{trackdata.sevenths_seg:0.2f}|\n"
-                display(Markdown(table_md))
-                plots.plotResults(chromadata[0],chromadata[1],chromadata[2],ground_truth[0],ground_truth[1],est_majmin[0],est_majmin[1])
+    def load_track(self):
+        display(self.filepaths,self.selection,self.output,clear=True)
+        self.trackdata = load_trackdata(os.path.join(self.path,self.dropdown_resultfile.value),
+                                                                            self.dropdown_id.value,self.dropdown_dataset.value)
+        trackdata,chromadata,_,_,_ = self.trackdata
+        # update t_max of slider 
+        self.t_start_slider.max = chromadata[0][-1]-15 # access track length via t_chroma from chromadata
+        self.display_trackdata(trackdata)
 
     def update_dataset(self,*args):
         """callback function for dropdown_dataset"""
         self.update_dropdown_id_options() # load new track_ids when dataset changed
-        self.dropdown_id.value = self.dropdown_id.options[0] # set default track_id when changing dataset
+        self.dropdown_id.value = None # set default track_id when changing dataset
 
     def update_dropdown_id_options(self,*args):
         self.dropdown_id.options = [x.track_id for x in self.trackdata if x.dataset == self.dropdown_dataset.value]
@@ -171,7 +193,7 @@ class hdf5GUI():
 
     def update_selected_track_id(self,*args):
         if self.dropdown_id.value is not None:
-            self.textbox_track_id.value = str(self.dropdown_id.value)
-            # print out name
+            self.load_track()
+            self.textbox_name.value = self.trackdata[0].name
         else:
-            self.textbox_track_id.value = "None"
+            self.textbox_name.value = "None"
