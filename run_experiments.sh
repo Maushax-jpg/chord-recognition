0bd97512d40bdd1ecd654c9886a78d01ff8aef4c
Maushax-jpg <maximilian.ederer@gmx.at>
2023-12-15 13:26:27 +0100

    shell script to create some data

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# parameters
-transition_probs=(0.2 0.3 0.4)
-eval_schemes=('majmin' 'sevenths')
-
-
-# Loop through combinations of filter lengths and transition probabilities
-for eval_scheme in "${eval_schemes[@]}"; do
-  for transition_prob in "${transition_probs[@]}"; do
-    dynamic_filename="${eval_scheme}_median_7_prob_$(echo $transition_prob | sed 's/\.//g')"
-    python transcribe.py "$dynamic_filename" --prefilter median --prefilter_length 7 --transition_prob "$transition_prob" --eval_scheme "$eval_scheme"
-  done
+# Loop through combinations of source separation techniques
+python transcribe.py "results/none_median_7_prob_025" --prefilter median --prefilter_length 7 --transition_prob "0.25"
+source_separation=("vocals" "drums" "both")
+for source_sep in "${source_separation[@]}"; do
+    dynamic_filename="results/${source_sep}_median_7_prob_025"
+    python transcribe.py "$dynamic_filename" --prefilter median --prefilter_length 7 --transition_prob "0.25" --source_separation "${source_sep}"
 done
