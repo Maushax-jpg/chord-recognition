bugfix log-likelihood

pitchspace.py
@@ -2,8 +2,6 @@ import numpy as np
 import utils
 from librosa.feature import tonnetz
 import features
-import train
-from scipy.signal import find_peaks
 
 def sym(n,g):
     '''SYM-operator described by Gatzsche and Mahnert'''
@@ -91,33 +89,6 @@ def computeHCDF(chroma,prefilter_length=3,use_cpss=True):
     )
     return harmonic_change
 
-def computeKeyCorrelation(chroma):
-    """ calculate correlation with major key profiles (krumhansl).
-    returns the most likey key for every time instance"""
-    templates = np.zeros((12,12),dtype=float)
-    key_profile = np.array([5, 2, 3.5, 2, 4.5, 4, 2, 4.5, 2, 3.5, 1.5, 4])
-    ic_energy = np.zeros_like(chroma)    
-    for i in range(12):
-        templates[i, :] = np.roll(key_profile,i)
-        # neglect all chromabins that are not part of the diatonic circle -> key_related_chroma
-        pitch_classes = np.roll([1,0,1,0,1,1,0,1,0,1,0,1],i)
-        key_related_chroma = np.multiply(chroma, np.reshape(pitch_classes, (12,1))) 
-        ic_energy[i, :] = np.sum(computeIntervalCategories(key_related_chroma), axis=0)
- 
-    correlation = np.matmul(templates,chroma) # matrix product (12x12) x (12 x N)
-    correlation_energy = np.square(correlation) + ic_energy
-    return correlation_energy
-
-def computeIntervalCategories(chroma):
-    if chroma.shape[0] != 12:
-        raise ValueError("invalid Chromagram shape!")
-    ic = np.zeros((6, chroma.shape[1]), dtype=float)
-
-    for i in range(6):
-        rotated_chroma = np.roll(chroma, shift=i+1, axis=0)
-        ic[i,:] = np.sum(chroma * rotated_chroma, axis=0)
-    return ic
-
 class ChordModel:
     """class for storing multivariate statistic of a chord in the pitch space system.
 
@@ -140,73 +111,49 @@ class ChordModel:
                 cov[j,k] = temp / (N - 1)
         return cov
 
-    def computeLogLikelyhood(self,x):
-        """evaluates the class likelyhood function at the specific point for both circles"""
-        # matrix product (x-mu)^T * Cov^(-1) * (x-mu)
-        res = ((x - self._mu).T @ np.linalg.inv(self._cov)) @ (x - self._mu)
-        return -0.5 * (np.log(np.linalg.det(self._cov)) + res + self._x.shape[0] * np.log(2*np.pi))
+    def computeLogLikelihood(self,x):
+        """evaluates the class log-likelihood function at the observation x"""
+        # pdf of a multivariate normal distribution evaluated at the observation x 
+        #  (2pi)^(-k/2)*det(Cov)^(-1/2)*exp(-1/2*(x-mu)^T * Cov^(-1) * (x-mu))
+        temp = ((x - self._mu).T @ np.linalg.inv(self._cov)) @ (x - self._mu)
+        logpdf = -(self._x.shape[0]/2) * np.log(2*np.pi) - 0.5 * np.log(np.linalg.det(self._cov)) -0.5*temp
+        if logpdf < 0:
+            pass
+        return logpdf
 
-class Classifier():
-    def __init__(self,filepath="/home/max/ET-TI/Masterarbeit/chord-recognition/models/chromadata_root_invariant_median.hdf5"): 
-        self._labels = utils.createChordTemplates("sevenths")[1]
-        self._model = self.createChordmodel(filepath)
+class CPSS_Classifier():
+    def __init__(self,alphabet="majmin",filepath="/home/max/ET-TI/Masterarbeit/chord-recognition/models/cpss",split=1): 
+        self._labels = utils.createChordTemplates(alphabet)[1]
+        self._key_templates = utils.createKeyTemplates()
+        self._model = self.loadChordmodel(filepath, alphabet, split)
 
-    def createChordmodel(self,filepath):
-        from tqdm import tqdm
-        chromadata = train.loadChromadata(filepath)
-        # offset in template array (see utils.createChordTemplates)
-        offsets = {"maj":0,"min":12,"maj7":24,"7":36,"min7":48} 
-        shift = [0,2,4,5,7,9]
-        keys = {}
-        for key_index in tqdm(range(12),desc=f"creating model for key"):
-            chordmodels = []
-            # create the triads for all major keys  
-            for i,qual in zip(shift,["maj","min","min","maj","maj","min"]):
-                chroma = np.roll(chromadata[qual],key_index + i,axis=0)
-                # transform onto pitchspace 
-                F,FR,TR,_ = computeCPSSfeatures(chroma)
-                # rearange features in a tuple
-                x = (
-                    F[0,:], F[1,:],
-                    FR[2 * key_index, :], FR[2*key_index+1, :],
-                    TR[2 * key_index, :], TR[2*key_index+1, :]
-                )
-                chord_index = (key_index + i) % 12 + offsets[qual]
-                chordmodels.append(ChordModel(chord_index, x))
+    def loadChordmodel(self,filepath,alphabet,split):
+        path = f"{filepath}/cpss_{alphabet}_{split}.npy"
+        return np.load(path,allow_pickle=True)[()]
 
-            # create the tetrads for all major keys  
-            for i,qual in zip(shift,["maj7","min7","min7","maj7","7","min7"]):
-                chroma = np.roll(chromadata[qual],key_index + i,axis=0)
-                # transform onto pitchspace 
-                F,FR,TR,_ = computeCPSSfeatures(chroma)
-                # rearange features in a tuple
-                x = (
-                    F[0,:], F[1,:],
-                    FR[2 * key_index, :], FR[2*key_index+1, :],
-                    TR[2 * key_index, :], TR[2*key_index+1, :]
-                )
-                chord_index = (key_index + i) % 12 + offsets[qual]
-                chordmodels.append(ChordModel(chord_index, x))
-            keys[key_index] = chordmodels
-        return keys
-
-    def computeLogLikelyhood(self,chromavector):
+    def computeLikelihoods(self,chromavector,key_index):
         likelyhood = np.zeros((len(self._labels),), dtype=float)
+        # compute feature vector for the selected key
         F,FR,TR,_ = computeCPSSfeatures(chromavector)
-        for key_index,key in self._model.items():
-            x = (
-                F[0,:], F[1,:],
-                FR[2 * key_index, :], FR[2*key_index+1, :],
-                TR[2 * key_index, :], TR[2*key_index+1, :]
-            )
-            for chordmodel in key:
-                temp = chordmodel.computeLogLikelyhood(x)
-                # only update likelyhood for a chord if it exceeds a previous calculation 
-                if temp > likelyhood[chordmodel._index]:
-                    likelyhood[chordmodel._index] = temp
+        x = (
+            F[0,:], F[1,:],
+            FR[2 * key_index, :], FR[2*key_index+1, :],
+            TR[2 * key_index, :], TR[2*key_index+1, :]
+        )
+        # compute likelyhood
+        for chordmodel in self._model[key_index]:
+            likelyhood[chordmodel._index] = chordmodel.computeLogLikelihood(x)
         return likelyhood
     
-    def classify(self,chromavector):
-        likelyhood = self.computeLogLikelyhood(chromavector)
-        index_ml = np.argmax(likelyhood)
-        return self._labels[index_ml],likelyhood[index_ml]
+    def selectKey(self, chromavector):
+        inner_product = np.matmul(self._key_templates.T, chromavector)
+        return np.argmax(inner_product)
+
+    def classify(self, chromavector,key_index):
+        likelyhood = self.computeLikelihoods(chromavector,key_index=key_index)
+        index = np.argmax(likelyhood)
+        # negative likelyhood -> this occurs if there is no good fit for the chromavector
+        if likelyhood[index] > 0:
+            return index
+        else:
+            return None